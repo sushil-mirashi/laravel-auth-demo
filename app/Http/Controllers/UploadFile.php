11fdb4b2ca34fc35ea03ec258d8a899be8bf1a43
Sushil-mirashi <sushilmirashi123@gmail.com>
2021-04-05 11:53:51 +0530

    uploadfile

--- app/Http/Controllers/UploadFile.php
+++ app/Http/Controllers/UploadFile.php
@@ -12,7 +12,9 @@ class UploadFile extends Controller
         // $request->all()->validate([
         //     'imageInput'=>'mimetypes:text/plain,png,jpg'
         // ]);
+        // dd(phpinfo());
         $file = $request->file('imageInput');
+        dd($_FILES['imageInput']['type'].'**'.$_FILES['imageInput']['tmp_name'].'**'.$_FILES['imageInput']['size'].'Error:'.$_FILES['imageInput']['error']);
         $filename = time() . '.' . $file->getClientOriginalExtension();
         $path1 = public_path().'/'.$filename;
         $path2 = public_path(). '/';
@@ -35,8 +37,42 @@ class UploadFile extends Controller
 
     public function getFile()
     {
+        $file_path = public_path().'new_folder';
+        if(\is_dir($file_path)){
+            $contents = scandir($file_path);
+            $contents = array_diff($contents, array('.', '..'));;
+            // \fopen($file_path.'test.txt', 'w+');
+            // \fclose();
+            $fileArr = array();
+            foreach(\glob($file_path."/*.txt") as $f){
+                $fileArr['name'] = basename($f); 
+                $fileArr['size'] = filesize($f).' bytes'; 
+            }
+            print_r($fileArr);
+        }else{
+            mkdir($file_path);
+            echo 'created';
+        }        
+        die();
+        $file = public_path().'/testScheduler.txt';
+        if(file_exists($file)){
+            // Attempt to open the file
+            $handle = fopen($file, "a+");     //required for fread
+            // $content = \fread($handle, \filesize($file));   //readfile func of php is easy way
+            // $content = \readfile($file);   //readfile func of php is easy way
+            // $content = \file_get_contents($file) or die('File not loaded');
+            //    fwrite($handle, 'hiiiiiiii');
+                \file_put_contents($file, ' HELOOOOOO', FILE_APPEND);
+               $content = file($file);
+            \fclose($handle);
+
+            print_r($content);
+        } else{
+            echo "ERROR: File does not exist.";
+        }
+        // \fopen($file, 'r+');
         // $content = Storage::get('storage/laravel.png');
-        return view('getfile');
+        // return view('getfile');
     }
 
     public function downloadfile(){
